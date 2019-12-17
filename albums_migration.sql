8e6edfe7c96bcad6fb14e8093f034d912c607b1d
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-17 15:49:27 -0600

    tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(100) NOT NULL,
-    release_date DATE,
-    sales INT UNSIGNED DEFAULT 0,
-    genre VARCHAR(100) DEFAULT 'NONE',
+    name VARCHAR(50) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DOUBLE UNSIGNED DEFAULT 0,
+    genre TEXT,
     PRIMARY KEY(id)
  );
