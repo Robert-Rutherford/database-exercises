7295f01648c8c83a233238afb0c8e62c5790093c
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-17 14:34:27 -0600

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL,
+    release_date DATE,
+    sales INT UNSIGNED DEFAULT 0,
+    genre VARCHAR(100) DEFAULT 'NONE'
+ );
