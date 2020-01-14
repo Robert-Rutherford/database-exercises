20104312f3246861665147fd6ec15d58815da58c
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-01-14 15:44:15 -0600

    exercise

--- setup.sql
+++ setup.sql
@@ -33,3 +33,28 @@ VALUES ('bob', 'bob@example.com', 1),
        ('adam', 'adam@example.com', 3),
        ('jane', 'jane@example.com', null),
        ('mike', 'mike@example.com', null);
+
+# code used to quickly create database tables for jdbc exercise in java 3 section
+# USE adlister_db;
+#
+# CREATE TABLE users(
+#                       id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#                       username VARCHAR(100) NOT NULL,
+#                       email VARCHAR(100) NOT NULL,
+#                       password VARCHAR(100) NOT NULL,
+#                       PRIMARY KEY (id)
+# )
+#
+# CREATE TABLE ads
+# (
+#     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     user_id INT UNSIGNED DEFAULT NULL,
+#     title VARCHAR(100) NOT NULL,
+#     description VARCHAR(500) NOT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (user_id) REFERENCES users (id)
+# );
+
+
+
+
